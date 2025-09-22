209a2b9a581bb9de0880285c6e13963367871273
Felix Tjandrawibawa <cemeng@gmail.com>
2025-09-22 11:41:17 +1000

    feat: add dry-run and help

--- cmd/renamer/main.go
+++ cmd/renamer/main.go
@@ -17,14 +17,41 @@ import (
 )
 
 func main() {
+	// Set custom usage message
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Renamer - A tool to organize photos and videos by their creation date\n\n")
+		fmt.Fprintf(os.Stderr, "Usage:\n")
+		fmt.Fprintf(os.Stderr, "  go run renamer/main.go -src=<source_dir>/ [-dest=<destination_dir>/] [-dry-run]\n\n")
+		fmt.Fprintf(os.Stderr, "Description:\n")
+		fmt.Fprintf(os.Stderr, "  Renamer processes photos and videos, organizing them by their creation date.\n")
+		fmt.Fprintf(os.Stderr, "  For photos (JPG, HEIC), it uses EXIF data to get the creation date.\n")
+		fmt.Fprintf(os.Stderr, "  For videos and other files (MOV, PNG, MP4, 3gp), it uses file modification time.\n")
+		fmt.Fprintf(os.Stderr, "  Files are renamed to: YYYY-MM-DD-HH-mm-SS-xxxx.ext format\n")
+		fmt.Fprintf(os.Stderr, "  where xxxx is a random suffix to prevent naming conflicts.\n\n")
+		fmt.Fprintf(os.Stderr, "Arguments:\n")
+		fmt.Fprintf(os.Stderr, "  -src    Source directory containing the files to process (required)\n")
+		fmt.Fprintf(os.Stderr, "  -dest   Destination directory for processed files (optional, defaults to source)\n")
+		fmt.Fprintf(os.Stderr, "  -dry-run Show what would be done without making any changes\n")
+		fmt.Fprintf(os.Stderr, "  -help   Show this help message\n\n")
+		fmt.Fprintf(os.Stderr, "Examples:\n")
+		fmt.Fprintf(os.Stderr, "  renamer -src=~/Photos/\n")
+		fmt.Fprintf(os.Stderr, "  renamer -src=~/Photos/ -dest=~/Organized/\n")
+		fmt.Fprintf(os.Stderr, "  renamer -src=~/Photos/ -dry-run\n\n")
+		fmt.Fprintf(os.Stderr, "Note: Directory paths must end with a trailing slash (/)\n")
+	}
+
 	var srcDirectory string
 	var destDirectory string
-	flag.StringVar(&srcDirectory, "src", "", "source directory")
-	flag.StringVar(&destDirectory, "dest", "", "destination directory")
+	var dryRun bool
+	flag.StringVar(&srcDirectory, "src", "", "source directory containing the files to process")
+	flag.StringVar(&destDirectory, "dest", "", "destination directory for processed files")
+	flag.BoolVar(&dryRun, "dry-run", false, "show what would be done without making actual changes")
 	flag.Parse()
 
-	if srcDirectory == "" {
-		log.Fatal("src argument is required, dest argument is optional, usage: renamer -src=xx/ -dest=xx/")
+	if srcDirectory == "" && !containsHelpFlag() {
+		fmt.Fprintf(os.Stderr, "Error: src argument is required\n\n")
+		flag.Usage()
+		os.Exit(1)
 	}
 	// By default the destination directory will be the same as source directory if it is not supplied via dest argument
 	if destDirectory == "" {
@@ -44,22 +71,21 @@ func main() {
 		if f.IsDir() || filename == ".DS_Store" {
 			continue
 		}
-		err := processFile(srcDirectory, destDirectory, filename)
+		err := processFile(srcDirectory, destDirectory, filename, dryRun)
 		if err != nil {
 			log.Fatalf("Error processing file %s: %s", filename, err)
 		}
 	}
 }
 
-func processFile(srcDirectory, destDirectory, fname string) error {
+func processFile(srcDirectory, destDirectory, fname string, dryRun bool) error {
 	result := strings.Split(fname, ".")
 	filename := result[0]
 	extension := result[1]
 
 	var destFilename string
 	var err error
-	switch extension {
-	case "JPG", "jpg", "HEIC":
+	if extension == "JPG" || extension == "jpg" || extension == "HEIC" {
 		destFilename, err = filenameFromExif(srcDirectory, filename, extension)
 		if err != nil {
 			// Getting filename from exif fails, use file attribute as failback
@@ -68,14 +94,23 @@ func processFile(srcDirectory, destDirectory, fname string) error {
 				return errors.Wrap(err, "Error getting filename from exif and attribute")
 			}
 		}
-	case "MOV", "mov", "PNG", "png", "MP4", "mp4", "3gp":
+	} else if extension == "MOV" || extension == "mov" || extension == "PNG" || extension == "png" || extension == "MP4" || extension == "mp4" || extension == "3gp" {
 		destFilename, err = filenameFromAttribute(srcDirectory, filename, extension)
 		if err != nil {
 			return errors.Wrap(err, "Error getting filename from attribute")
 		}
-	default:
+	} else {
 		fmt.Printf("Ignoring file with unsupported extension: %s\n", fname)
-		// return errors.New("Cannot handle file with extension " + extension)
+		return nil
+	}
+
+	if dryRun {
+		fmt.Printf("[DRY-RUN] Would rename:\n")
+		fmt.Printf("  Source: %s\n", filepath.Join(srcDirectory, fname))
+		fmt.Printf("  Destination: %s\n", filepath.Join(destDirectory, destFilename))
+		fmt.Printf("  Then move source to: %s\n", filepath.Join(srcDirectory, "processed", fname))
+		fmt.Println("---")
+		return nil
 	}
 
 	// Copy the file to destination
@@ -153,6 +188,16 @@ func timeToFilename(time time.Time, extension string) string {
 }
 
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+// containsHelpFlag checks if -h, -help, or --help is present in command line arguments
+func containsHelpFlag() bool {
+	for _, arg := range os.Args[1:] {
+		if arg == "-h" || arg == "-help" || arg == "--help" {
+			return true
+		}
+	}
+	return false
+}
 
 func randomSuffix(n int) string {
 	b := make([]byte, n)
